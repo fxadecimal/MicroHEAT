ea4f061c2d8f8d18f562b53d1812fc994cf0f673
fxadecimal <fxadecimal@users.noreply.github.com>
2025-01-05 13:39:54 +0000

    added sample heating agent script

---
+++ sample_heat_agent.py
@@ -0,0 +1,32 @@
+"""
+
+Sample heating agent that switches the heating on and off based on the thermostat temperature.
+
+"""
+
+import time
+import requests
+
+API_URL = "http://localhost:8080"
+SLEEP = 10
+
+# reset the simulation
+response = requests.get(f"{API_URL}/reset")
+response.raise_for_status()
+
+while True:
+    response = requests.get(f"{API_URL}/sensor")
+    temp = response.json()["value"]
+
+    # get the thermostat state
+    response = requests.get(f"{API_URL}/thermostat")
+    thermostat = response.json()["value"]
+    print(f"Current temperature: {temp}, Thermostat: {thermostat}")
+
+    # switch the heating on or off based on the thermostat state
+    if thermostat == True:
+        response = requests.get(f"{API_URL}/heat-source/off")
+    else:
+        response = requests.get(f"{API_URL}/heat-source/on")
+
+    time.sleep(SLEEP)
